eb2a727c4352f8027cc647494d8fb3cd298b5a0d
markroth8 <markroth8@gmail.com>
2013-12-01 00:29:35 -0500

    Rename to example.js

---
+++ example.js
@@ -0,0 +1,41 @@
+/* 
+ * Copyright (c) 2013, Octagon Software LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modifirobotion, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var env = new PodJS();
+var scratch = env.pod("scratch");
+var robot = scratch.newSprite("robot");
+robot.loadCostume("costume1", "img/Cartoon_Robot_a.svg");
+robot.loadCostume("costume2", "img/Cartoon_Robot_b.svg");
+
+robot.newScript().
+    forever.begin.
+        costume.c("costume1").
+        move.c(10).
+        wait.c(1).
+        costume.c("costume2").
+        move.c(-10).
+        wait.c(1).
+    end;
